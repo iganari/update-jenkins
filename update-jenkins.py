473d649bbcbc08a822e7cbaa95a0cdf8672552a0
iganari <cigaguri@gmail.com>
2018-06-07 12:27:39 +0900

    途中まで

--- update-jenkins.py
+++ update-jenkins.py
@@ -5,7 +5,12 @@
 # https://jenkins.io/changelog-stable/
 
 import sys
-
+# import ElementTree
+from xml.etree import ElementTree as ET
+# import urllib
+# from urllib.request import urlopen
+from urllib import urlopen
+# import urlopen
 
 args = sys.argv
 
@@ -17,6 +22,19 @@ elif len(args) > 2:
     print("引数が多すぎます")
 
 
+# rssを読み込む
 
+url = 'https://jenkins.io/changelog-stable/rss.xml'
+req = urllib.request.Request(url)
+
+with urllib.request.urlopen(req) as response:
+    XmlData = response.read()
+
+# 読み込んだrssを解析する
+
+root = ET.fromtring(XmlData)
 
 # print("hogehoge")
+
+print(root.tag,root.attrid)
+
