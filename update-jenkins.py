c1d2896825992dd752fe7da3931b2e389b70d5e3
iganari <cigaguri@gmail.com>
2018-06-07 11:55:51 +0900

    引数のチェックをいれる

--- update-jenkins.py
+++ update-jenkins.py
@@ -4,4 +4,19 @@
 ### Doc
 # https://jenkins.io/changelog-stable/
 
-print("hogehoge")
+import sys
+
+
+args = sys.argv
+
+if len(args) == 1:
+    print("引数がありません")
+elif len(args) == 2:
+    print(args[1])
+elif len(args) > 2:
+    print("引数が多すぎます")
+
+
+
+
+# print("hogehoge")
