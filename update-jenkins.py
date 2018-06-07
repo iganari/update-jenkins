0e92ba161a27e2b181a6e81433c75ad1e7da53af
iganari <cigaguri@gmail.com>
2018-06-08 08:26:58 +0900

    モジュール化

--- update-jenkins.py
+++ update-jenkins.py
@@ -11,42 +11,64 @@
 
 import sys
 
-
-args = sys.argv
-
-if len(args) == 1:
-    print("引数を入れてください")
-elif len(args) == 2:
-    print(args[1])
+def rss_url():
     if args[1] == 'lts':
-        RSS_URL = 'https://jenkins.io/changelog-stable/rss.xml'
+        rss_url = 'https://jenkins.io/changelog-stable/rss.xml'
     elif args[1] == 'latest':
-        RSS_URL = 'https://jenkins.io/changelog/rss.xml'
+        rss_url = 'https://jenkins.io/changelog/rss.xml'
     else:
         print('引数が不正です')
         sys.exit(1)
-elif len(args) > 2:
-    print("引数が多すぎます")
+    return rss_url
 
-print(RSS_URL)
+if __name__ == '__main__':
+    
+    args = sys.argv
+    
+    if len(args) == 1:
+        print("引数を入れてください")
+        sys.exit(1)
+    elif len(args) == 2:
+        print(args[1])
+        # if args[1] == 'lts':
+        #     RSS_URL = 'https://jenkins.io/changelog-stable/rss.xml'
+        # elif args[1] == 'latest':
+        #     RSS_URL = 'https://jenkins.io/changelog/rss.xml'
+        # else:
+        #     print('引数が不正です')
+        #     sys.exit(1)
+        RSS_URL = rss_url()
+    elif len(args) > 2:
+        print("引数が多すぎます")
+        sys.exit(1)
 
-import feedparser
+    print(RSS_URL)
+    
+    import feedparser
+    
+    jenkins_lts_dic = feedparser.parse(RSS_URL)
+    
+    # print (jenkins_lts_dic.feed.title)
+    # print (jenkins_lts_dic.feed.title_detail)
+    # print (jenkins_lts_dic.feed.updated)
+    # print (jenkins_lts_dic.feed.item[0].title)
+    # print (jenkins_lts_dic.feed)
+    # print (jenkins_lts_dic)
+    print (jenkins_lts_dic.entries[0].title, jenkins_lts_dic.entries[0].link)
+    print (jenkins_lts_dic.entries[0].link)
+    
+    lts_link = jenkins_lts_dic.entries[0].link
+    print(lts_link)
+    
+    lts_ver = lts_link.split("#") 
+    print (lts_ver)
+    print (lts_ver[1])
 
-jenkins_lts_dic = feedparser.parse(RSS_URL)
 
-# print (jenkins_lts_dic.feed.title)
-# print (jenkins_lts_dic.feed.title_detail)
-# print (jenkins_lts_dic.feed.updated)
-# print (jenkins_lts_dic.feed.item[0].title)
-# print (jenkins_lts_dic.feed)
-# print (jenkins_lts_dic)
-print (jenkins_lts_dic.entries[0].title, jenkins_lts_dic.entries[0].link)
-print (jenkins_lts_dic.entries[0].link)
-
-lts_link = jenkins_lts_dic.entries[0].link
-print(lts_link)
-
-lts_ver = lts_link.split("#") 
-print (lts_ver)
-print (lts_ver[1])
-
+# def test():
+#     comment = "iganari test"
+#     return comment
+# 
+# if __name__ == '__main__':
+#     s = test()
+#     print(s)
