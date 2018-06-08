592a8aae40d8d9153d177a7120fb38032d793a49
iganari <cigaguri@gmail.com>
2018-06-08 15:06:51 +0900

    update

--- update-jenkins.py
+++ update-jenkins.py
@@ -2,12 +2,11 @@
 # cording: utf-8
 
 ### Long-term Support (LTS) 
-# https://jenkins.io/changelog-stable/
 # https://jenkins.io/changelog-stable/rss.xml
 
 ### Weekly
-# https://jenkins.io/changelog/
 # https://jenkins.io/changelog/rss.xml
+
 
 import sys
 
@@ -21,7 +20,7 @@ def rss_url():
         sys.exit(1)
     return rss_url
 
-def jenkins_ver():
+def chk_jenkins_ver():
     import feedparser
 
     url = rss_url()
@@ -35,6 +34,9 @@ def jenkins_ver():
 
 def chk_war_file():
     import os
+    # import datetime
+    # import shutil
+
     jenkins_war_dir  = '/usr/lib/jenkins'
     jenkins_war_file = jenkins_war_dir + '/jenkins.war'
 
@@ -43,29 +45,42 @@ def chk_war_file():
             print('OK')
         else:
             print ('must create link')
+            # print(jenkins_war_dir + '/' + jenkins_ver)
+            # os.mkdir(jenkins_war_dir + '/' + jenkins_ver)
+            # os.mkdir(jenkins_war_dir + '/backup_' + datetime.datetime.today().strftime("%Y%m%d"))
     else:
-        print("must create")
+        print("PATHが間違っているか、そもそもインストールされてない可能性があります")
+        sys.exit(1)
+
+
+
+
 
 
 if __name__ == '__main__':
-    
-
-
-    
     args = sys.argv
-    
     if len(args) == 1:
         print("引数を入れてください")
         sys.exit(1)
     elif len(args) == 2:
         print(args[1])
+
+        jenkins_ver = chk_jenkins_ver()
+        print(jenkins_ver)
+
         RSS_URL = rss_url()
+        print(RSS_URL)
+
+        chk_war_file()
+
+        get_jenkins_war()
+
     elif len(args) > 2:
         print("引数が多すぎます")
         sys.exit(1)
-    print(RSS_URL)
+    # print(RSS_URL)
     
 
     
-    jenkins_ver = jenkins_ver()
-    print(jenkins_ver)
+    # jenkins_ver = jenkins_ver()
+    # print(jenkins_ver)
